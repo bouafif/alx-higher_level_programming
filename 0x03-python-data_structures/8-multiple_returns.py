5d89e584667b0c2030d2bfa14b1c29900d2a734a
bouafif <bouafifwael12@gmail.com>
2023-03-13 10:08:56 -0700

    8. More returns!

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """returns a tuple with the length of a string and its first character."""
+
+    length = len(sentence)
+    if length == 0:
+        first = None
+    first = sentence[0]
+    return (len(sentence), sentence[0])
