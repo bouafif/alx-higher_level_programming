810a433e14915e51ce14b4e9840c42466f521d35
bouafif <bouafifwael12@gmail.com>
2023-03-13 10:10:41 -0700

    8. More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,6 +4,6 @@ def multiple_returns(sentence):
 
     length = len(sentence)
     if length == 0:
-        first = None
+        return (0, None)
     first = sentence[0]
     return (len(sentence), sentence[0])
