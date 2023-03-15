3bd9eee47b6e9fa8f5ed477d4c9cd2d7df9515fc
bouafif <bouafifwael12@gmail.com>
2023-03-15 15:09:09 -0700

    8-simple_delete.py

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,7 @@
+#!/bin/usr/pyhton3
+def simple_delete(a_dictionary, key=""):
+    """deletes a key in a dictionary."""
+
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
