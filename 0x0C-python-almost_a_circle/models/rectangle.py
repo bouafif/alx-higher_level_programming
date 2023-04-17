d2fc50a3bdae94a7a5d9c6b1741cffeb9972e13c
bouafif <bouafifwael12@gmail.com>
2023-04-17 10:03:29 -0700

    8. Update #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -162,3 +162,19 @@ class Rectangle(Base):
             str: Formatted string representation of the rectangle.
         """
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+
+    def update(self, *args):
+        """
+        Assign provided arguments to the respective attributes in the order:
+        id, width, height, x, y.
+        """
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
